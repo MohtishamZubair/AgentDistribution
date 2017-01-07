5d561571443b3c93abf79e620606435ae539abdf
Mohtisham Zubair <mohtishamzubair@gmail.com>
2017-01-08 01:44:11 +0500

    Angularjs2 enable (#12)
    * gitIgnore4Angular
    
    Setting diff. files and folder to ignore list for angularjs2
    
    * angularjs2Enabled
    
    closes #2
    Added the angularjs2 into the MVC project. Now project need to be
    compiled and then its changes can be reflected
    
    * enableAucompilebytsjson
    
    #2
    2.1 #10  #11
    Now no need to compile for TS file autocompile is enable within VS2015

--- src/AgentDistribution.FrontEnd/TS/test.js
+++
@@ -1,5 +0,0 @@
-function saySalam() {
-    var compiler = document.getElementById("compiler").value;
-    var framework = document.getElementById("framework").value;
-    return "Salam from " + compiler + " and " + framework + "!";
-}
--- src/AgentDistribution.FrontEnd/TS/test.ts
+++
@@ -1,5 +0,0 @@
-﻿function setTextTS() {
-    const compiler = (document.getElementById("compiler") as HTMLInputElement).value;
-    const framework = (document.getElementById("framework") as HTMLInputElement).value;
-    return `Salam from ${compiler} and ${framework}!`;
-}
