fc041e7ba97e3d36adfb903c38b1034e3208fb1d
mohtisham <mohtishamzubair@gmail.com>
2017-01-07 15:10:09 +0500

    FrontEndProject
    #2
    2.1
    #10 this has been setup following
    
    https://github.com/Microsoft/TypeScript-Handbook/blob/master/pages/tutorials/ASP.NET%204.md

---
+++ src/AgentDistribution.FrontEnd/TS/test.ts
@@ -0,0 +1,5 @@
+﻿function setTextTS() {
+    const compiler = (document.getElementById("compiler") as HTMLInputElement).value;
+    const framework = (document.getElementById("framework") as HTMLInputElement).value;
+    return `Salam from ${compiler} and ${framework}!`;
+}
